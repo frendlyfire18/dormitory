52383e84573d652f81b4a4f7fca4e2e6f745620a
frendly.fire2017 <frendly.fire2017@gmail.com>
2022-12-05 23:43:14 +0300

    first commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const uri = "mongodb+srv://admin:admin@givemeburgerdb.kyuig.mongodb.net/givemeburger?retryWrites=true&w=majority&ssl=true"
 export const options = {}
-export const cur_url_with_host_and_port = `http://localhost:3000/`;
+export const cur_url_with_host_and_port = `https://dormitory-nine.vercel.app/`;
 module.exports={
     uri,
     cur_url_with_host_and_port,
